27f63a72216ba60f6b122d84c73b91d0c901a679
5zig <alex.albers@me.com>
2014-10-12 19:45:19 +0200

    Debug message for spawn spritesheet

--- src/plattformer/level/SpawnLevel.java
+++ src/plattformer/level/SpawnLevel.java
@@ -17,13 +17,16 @@ public class SpawnLevel extends Level {
 
 	@Override
 	protected void loadLevel() {
+		System.out.print("Trying to load Spritesheet from " + path + "... ");
 		try {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			width = image.getWidth();
 			height = image.getHeight();
 			tiles = new int[width * height];
 			image.getRGB(0, 0, width, height, tiles, 0, width > height ? width : height);
+			System.out.println("done.");
 		} catch (IOException e) {
+			System.out.println("failed.");
 			e.printStackTrace();
 		}
 	}
