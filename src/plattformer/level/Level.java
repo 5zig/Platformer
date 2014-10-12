18afc29c459931a2e0e05197666e9df66dc06589
5zig <alex.albers@me.com>
2014-10-12 11:25:25 +0200

    Added render & update methods in tile class

--- src/plattformer/level/Level.java
+++ src/plattformer/level/Level.java
@@ -1,15 +1,27 @@
 package plattformer.level;
 
 import plattformer.Game;
+import plattformer.level.tiles.Tile;
 
 public class Level {
 
 	private Game game;
-	
+	private int width, height;
+
+	private int[] tiles;
+
 	public Level(Game game) {
 		this.game = game;
 	}
-	
-	
-	
+
+	public void render() {
+	}
+
+	public void update() {
+	}
+
+	public Tile getTile(int x, int y) {
+		return null;
+	}
+
 }
