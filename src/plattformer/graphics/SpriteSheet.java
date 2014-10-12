07291b0f47ad0a2c99ddcedee203e6f78a5e790f
5zig <alex.albers@me.com>
2014-10-12 19:33:45 +0200

    Added Spritesheet class

---
+++ src/plattformer/graphics/SpriteSheet.java
@@ -0,0 +1,49 @@
+package plattformer.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+
+	private String path;
+	private int width, height;
+	private int[] pixels;
+
+	public static final SpriteSheet TEST = new SpriteSheet("/test.png");
+	
+	public static final SpriteSheet PLAYER = new SpriteSheet("/entities/player.png");
+
+	public SpriteSheet(String path) {
+		this.path = path;
+		load();
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int[] getPixels() {
+		return pixels;
+	}
+
+	private void load() {
+		System.out.print("Trying to load Spritesheet from " + path + "... ");
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			width = image.getWidth();
+			height = image.getHeight();
+			pixels = new int[width * height];
+			image.getRGB(0, 0, width, height, pixels, 0, width > height ? width : height);
+			System.out.println("done.");
+		} catch (IOException e) {
+			System.out.println("failed.");
+			e.printStackTrace();
+		}
+	}
+}
