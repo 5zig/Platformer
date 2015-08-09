6f672b1044de963f32feae9fc46bbd6a24aa4899
Niki1337 <iniki1337@googlemail.com>
2015-08-10 00:07:41 +0200

    Added more tiles & adjusted player physics

--- src/plattformer/level/tiles/Tile.java
+++ src/plattformer/level/tiles/Tile.java
@@ -6,12 +6,28 @@ import plattformer.screens.Screen;
 
 public class Tile {
 
+	public static final Tile VOID = new Tile(Assets.VOID, false);
+
 	// Tileset: Overworld Level
 	public static final Tile GRASS = new Tile(Assets.GRASS, true);
+	public static final Tile DIRT = new Tile(Assets.DIRT, true);
 	public static final Tile GRASS_LEFT = new Tile(Assets.GRASS_LEFT, false);
 	public static final Tile GRASS_RIGHT = new Tile(Assets.GRASS_RIGHT, false);
-	public static final Tile DIRT = new Tile(Assets.DIRT, true);
-	public static final Tile VOID = new Tile(Assets.VOID, false);
+	public static final Tile DIRT_LEFT = new Tile(Assets.DIRT_LEFT, false);
+	public static final Tile DIRT_RIGHT = new Tile(Assets.DIRT_RIGHT, false);
+	public static final Tile DIRT_ABOVE = new Tile(Assets.DIRT_ABOVE, false);
+	public static final Tile DIRT_ABOVE_LEFT = new Tile(Assets.DIRT_ABOVE_LEFT, false);
+	public static final Tile DIRT_ABOVE_RIGHT = new Tile(Assets.DIRT_ABOVE_RIGHT, false);
+	public static final Tile DIRTGRASS_LEFT = new Tile(Assets.DIRTGRASS_LEFT, true);
+	public static final Tile DIRTGRASS_RIGHT = new Tile(Assets.DIRTGRASS_RIGHT, true);
+	public static final Tile DIRTGRASS_BOTH = new Tile(Assets.DIRTGRASS_BOTH, true);
+	public static final Tile DIRTGRASS_GAP = new Tile(Assets.DIRTGRASS_GAP, false);
+	public static final Tile FLOWER_1 = new Tile(Assets.FLOWER_1, false);
+	public static final Tile FLOWER_2 = new Tile(Assets.FLOWER_2, false);
+	public static final Tile FLOWER_3 = new Tile(Assets.FLOWER_3, false);
+	public static final Tile ISLAND_LEFT = new Tile(Assets.ISLAND_LEFT, true);
+	public static final Tile ISLAND_MID = new Tile(Assets.ISLAND_MID, true);
+	public static final Tile ISLAND_RIGHT = new Tile(Assets.ISLAND_RIGHT, true);
 
 	private Sprite sprite;
 	private boolean solid;
--- src/plattformer/level/tiles/Tileset.java
+++ src/plattformer/level/tiles/Tileset.java
@@ -9,7 +9,9 @@ public class Tileset {
     private Tile[] tiles;
     private int tileSize;
 
-    public static Tileset OVERWORLD_LEVEL = new Tileset(Assets.OVERWORLD_LEVEL_BG, 16, Tile.GRASS, Tile.DIRT, Tile.GRASS_LEFT, Tile.GRASS_RIGHT);
+    public static Tileset OVERWORLD_LEVEL = new Tileset(Assets.OVERWORLD_LEVEL_BG, 16, Tile.VOID, Tile.GRASS, Tile.DIRT, Tile.GRASS_LEFT, Tile.GRASS_RIGHT,
+            Tile.DIRT_LEFT, Tile.DIRT_RIGHT, Tile.DIRT_ABOVE, Tile.DIRT_ABOVE_LEFT, Tile.DIRT_ABOVE_RIGHT, Tile.DIRTGRASS_LEFT, Tile.DIRTGRASS_RIGHT,
+            Tile.DIRTGRASS_BOTH, Tile.DIRTGRASS_GAP, Tile.FLOWER_1, Tile.FLOWER_2, Tile.FLOWER_3, Tile.ISLAND_LEFT, Tile.ISLAND_MID, Tile.ISLAND_RIGHT);
 
     public Tileset(Sprite background, int tileSize, Tile... tiles) {
         this.background = background;
@@ -26,10 +28,9 @@ public class Tileset {
     }
 
     public Tile getTile(int id) {
-        int newid = id - 1; // Tile 0 should always be void
-        if (newid < 0 || newid >= tiles.length)
+        if (id < 0 || id >= tiles.length)
             return Tile.VOID;
-        return tiles[newid];
+        return tiles[id];
     }
 
     public int getTileSize() {
